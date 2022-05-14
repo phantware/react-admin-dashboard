6f84885243b55326254b7fe0eb2286fcea9e620d
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-14 16:39:05 +0100

    all MUI icons added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import './sidebar.scss'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined'
+import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined'
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined'
+import LocalShippingIcon from '@mui/icons-material/LocalShipping'
+import AssessmentIcon from '@mui/icons-material/Assessment'
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined'
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined'
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined'
+import SettingsApplicationsOutlinedIcon from '@mui/icons-material/SettingsApplicationsOutlined'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
+import ExitToAppTwoToneIcon from '@mui/icons-material/ExitToAppTwoTone'
 
 const Sidebar = () => {
   return (
@@ -10,9 +22,54 @@ const Sidebar = () => {
       <hr />
       <div className='center'>
         <ul>
-          <li>Dashboard</li>
-          <li>Dashboard</li>
-          <li>Dashboard</li>
+          <li>
+            <DashboardIcon />
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <PersonOutlineOutlinedIcon />
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreOutlinedIcon />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardOutlinedIcon />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon />
+            <span>Delivery</span>
+          </li>
+          <li>
+            <AssessmentIcon />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsOutlinedIcon />
+            <span>Notifications</span>
+          </li>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsOutlinedIcon />
+            <span>Settings</span>
+          </li>
+          <li>
+            <AccountCircleOutlinedIcon />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppTwoToneIcon />
+            <span>Logout</span>
+          </li>
         </ul>
       </div>
       <div className='bottom'>color options</div>
