e2228af4d6b0a5b12a1665bd5a8b091f81850c31
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-14 15:31:47 +0100

    side bar implementation process

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 import './sidebar.scss'
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    <div className='sidebar'>
+      <div className='top'>
+        <span className='logo'>Phantware</span>
+      </div>
+      <hr />
+      <div className='center'>
+        <ul>
+          <li>Dashboard</li>
+          <li>Dashboard</li>
+          <li>Dashboard</li>
+        </ul>
+      </div>
+      <div className='bottom'>color options</div>
+    </div>
+  )
 }
 
 export default Sidebar
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './home.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
-  return <div className='home'>Home</div>
+  return (
+    <div className='home'>
+      <Sidebar />
+      <div className='homeContainer'>Home Container</div>
+    </div>
+  )
 }
 
 export default Home
