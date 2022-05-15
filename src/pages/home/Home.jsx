93c3e6dea3fe98c4c6c7163b08d02a0930f25830
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-15 12:18:57 +0100

    navbar implemented

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './home.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 
 const Home = () => {
   return (
     <div className='home'>
       <Sidebar />
-      <div className='homeContainer'>Home Container</div>
+      <div className='homeContainer'>
+        <Navbar />
+        Home Container
+      </div>
     </div>
   )
 }
