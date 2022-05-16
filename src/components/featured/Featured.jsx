fcd5ffe7aba994256fbceddabf033024cf4683ff
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-16 13:11:49 +0100

    react-circular-progressbar package added

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './featured.scss'
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined'
 
 const Featured = () => {
   return (
     <div className='featured'>
       <div className='top'>
         <h1 className='title'>Total Revenue</h1>
+        <MoreVertOutlinedIcon fontSize='small' />
       </div>
-      <div className='bottom'>bottom</div>
+      <div className='bottom'>
+        <div className='featuredChart'></div>
+      </div>
     </div>
   )
 }
