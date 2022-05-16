e2374a524f96ed8fd6a06abb9dc164a165ff529b
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-16 16:25:01 +0100

    progress bar fully implemented

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,6 +3,8 @@ import './featured.scss'
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined'
 import { CircularProgressbar } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 
 const Featured = () => {
   return (
@@ -13,7 +15,36 @@ const Featured = () => {
       </div>
       <div className='bottom'>
         <div className='featuredChart'>
-          <CircularProgressbar value={70} text={'70%'} />
+          <CircularProgressbar value={90} text={'90%'} strokeWidth={3} />
+        </div>
+        <p className='title'>Total Sales made today</p>
+        <p className='amount'>$420</p>
+        <p className='desc'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum porro,
+          odio modi eveniet molestiae est id quas beatae officiis.
+        </p>
+        <div className='summary'>
+          <div className='item'>
+            <div className='itemTitle'>Target</div>
+            <div className='itemResult negative'>
+              <KeyboardArrowDownIcon fontSize='small' />
+              <div className='resultAmount'>$12.4k</div>
+            </div>
+          </div>
+          <div className='item'>
+            <div className='itemTitle'>Last Week</div>
+            <div className='itemResult positive'>
+              <KeyboardArrowUpIcon fontSize='small' />
+              <div className='resultAmount'>$12.4k</div>
+            </div>
+          </div>
+          <div className='item'>
+            <div className='itemTitle'>Last Month</div>
+            <div className='itemResult positive'>
+              <KeyboardArrowUpIcon fontSize='small' />
+              <div className='resultAmount'>$12.4k</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
