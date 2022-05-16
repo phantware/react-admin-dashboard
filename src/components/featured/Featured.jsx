75a66cdefc8e11aa98700af8afa465d2258eb175
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-16 12:39:40 +0100

    chart session continues

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './featured.scss'
 
 const Featured = () => {
-  return <div className='featured'>Featured</div>
+  return (
+    <div className='featured'>
+      <div className='top'>
+        <h1 className='title'>Total Revenue</h1>
+      </div>
+      <div className='bottom'>bottom</div>
+    </div>
+  )
 }
 
 export default Featured
