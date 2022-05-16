400df1d0c4374a56c2442aeed5f9126309d25648
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-16 11:19:01 +0100

    chart and featured components added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
 import Widget from '../../components/widget/Widget'
+import Featured from '../../components/featured/Featured'
+import Chart from '../../components/chart/Chart'
 
 const Home = () => {
   return (
@@ -16,6 +18,10 @@ const Home = () => {
           <Widget type='order' />
           <Widget type='earning' />
           <Widget type='balance' />
+        </div>
+        <div className='charts'>
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>
