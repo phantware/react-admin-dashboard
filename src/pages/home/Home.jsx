0401a5e6c99c2d33ff989b06328d3716291fe8c3
phantware <phantmoney2011@gmail.com>
2023-05-04 17:04:20 +0100

    chart.jsx file modified

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import './home.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Sidebar from '../../components/sidebar/Sidebar'
@@ -8,8 +8,44 @@ import Featured from '../../components/featured/Featured'
 import Chart from '../../components/chart/Chart'
 import WidgetLg from '../../components/widgetLg/WidgetLg'
 import WidgetSm from '../../components/widgetSm/WidgetSm'
+import axiosInstance from '../../utils/requestMethods'
 
 const Home = () => {
+  const [userStats, setUserStats] = useState([])
+
+  const MONTHS = useMemo(
+    () => [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Agu',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],
+    []
+  )
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await axiosInstance.get('/users/stats')
+        res.data.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], 'Active User': item.total },
+          ])
+        )
+      } catch {}
+    }
+    getStats()
+  }, [MONTHS])
+
   return (
     <div className='home'>
       <Sidebar />
@@ -23,7 +59,12 @@ const Home = () => {
         </div>
         <div className='charts'>
           <Featured />
-          <Chart />
+          <Chart
+            data={userStats}
+            title='User Analytics'
+            grid
+            dataKey='Active User'
+          />
         </div>
         <div className='charts'>
           <WidgetSm />
