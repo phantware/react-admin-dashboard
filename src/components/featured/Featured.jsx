78de3f58227164b99820a641a9b6ef6f6e9a28c1
phantware <phantmoney2011@gmail.com>
2023-05-04 15:22:07 +0100

    Featured.js file modified

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './featured.scss'
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined'
 import { CircularProgressbar } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import { axiosInstance } from '../../utils/requestMethods'
 
 const Featured = () => {
+  const [income, setIncome] = useState([])
+  const [perc, setPerc] = useState(0)
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await axiosInstance.get('orders/income')
+        setIncome(res.data)
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100)
+      } catch {}
+    }
+    getIncome()
+  }, [])
+
   return (
     <div className='featured'>
       <div className='top'>
@@ -18,11 +33,8 @@ const Featured = () => {
           <CircularProgressbar value={90} text={'90%'} strokeWidth={3} />
         </div>
         <p className='title'>Total Sales made today</p>
-        <p className='amount'>$420</p>
-        <p className='desc'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum porro,
-          odio modi eveniet molestiae est id quas beatae officiis.
-        </p>
+        <p className='amount'>${income[1]?.total || 0}</p>
+        <p className='desc'>Above is the total sales made today</p>
         <div className='summary'>
           <div className='item'>
             <div className='itemTitle'>Target</div>
