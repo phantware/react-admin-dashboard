7e37ce6ecb4d84f40b1135a6ea9dac8c58a49751
phantware <phantmoney2011@gmail.com>
2023-05-05 16:47:50 +0100

    packages removed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { login } from '../../redux/apiCalls'
-import '../login/login.scss'
+import './login.scss'
 
 const Login = () => {
   const [username, setUsername] = useState('')
