16cb423c27de314abb40c1e3679c505d75c80cf4
phantware <phantmoney2011@gmail.com>
2023-05-05 13:15:36 +0100

    empty folders deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import List from './pages/list/List'
-import Single from './pages/single/Single'
-import New from './pages/new/New'
 
 function App() {
   return (
@@ -14,14 +11,14 @@ function App() {
             <Route index element={<Home />} />
             <Route path='login' element={<Login />} />
             <Route path='users'>
-              <Route index element={<List />} />
-              <Route path=':userId' element={<Single />} />
-              <Route path='new' element={<New />} />
+              {/* <Route index element={<List />} /> */}
+              {/* <Route path=':userId' element={<Single />} /> */}
+              {/* <Route path='new' element={<New />} /> */}
             </Route>
             <Route path='products'>
-              <Route index element={<List />} />
-              <Route path=':productId' element={<Single />} />
-              <Route path='new' element={<New />} />
+              {/* <Route index element={<List />} /> */}
+              {/* <Route path=':productId' element={<Single />} /> */}
+              {/* <Route path='new' element={<New />} /> */}
             </Route>
           </Route>
         </Routes>
