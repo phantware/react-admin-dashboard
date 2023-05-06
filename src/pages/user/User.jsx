c2346056251067b0d2e3b6abb73e788922783828
phantware <phantmoney2011@gmail.com>
2023-05-06 10:48:21 +0100

    userList modified

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import {
-  CalendarToday,
-  LocationSearching,
-  MailOutline,
-  PermIdentity,
-  PhoneAndroid,
-  Publish,
-} from '@material-ui/icons'
 import { Link } from 'react-router-dom'
 import './user.css'
+import { PermIdentity } from '@mui/icons-material'
 
 const User = () => {
   return (
     <div className='user'>
       <div className='userTitleContainer'>
         <h1 className='userTitle'>Edit User</h1>
-        <Link to='/newUser'>
+        <Link to='/new'>
           <button className='userAddButton'>Create</button>
         </Link>
       </div>
@@ -38,23 +31,23 @@ const User = () => {
               <PermIdentity className='userShowIcon' />
               <span className='userShowInfoTitle'>annabeck99</span>
             </div>
-            <div className='userShowInfo'>
+            {/* <div className='userShowInfo'>
               <CalendarToday className='userShowIcon' />
               <span className='userShowInfoTitle'>10.12.1999</span>
-            </div>
+            </div> */}
             <span className='userShowTitle'>Contact Details</span>
-            <div className='userShowInfo'>
+            {/* <div className='userShowInfo'>
               <PhoneAndroid className='userShowIcon' />
               <span className='userShowInfoTitle'>+1 123 456 67</span>
-            </div>
-            <div className='userShowInfo'>
+            </div> */}
+            {/* <div className='userShowInfo'>
               <MailOutline className='userShowIcon' />
               <span className='userShowInfoTitle'>annabeck99@gmail.com</span>
-            </div>
-            <div className='userShowInfo'>
+            </div> */}
+            {/* <div className='userShowInfo'>
               <LocationSearching className='userShowIcon' />
               <span className='userShowInfoTitle'>New York | USA</span>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className='userUpdate'>
@@ -110,7 +103,7 @@ const User = () => {
                   alt=''
                 />
                 <label htmlFor='file'>
-                  <Publish className='userUpdateIcon' />
+                  {/* <Publish className='userUpdateIcon' /> */}
                 </label>
                 <input type='file' id='file' style={{ display: 'none' }} />
               </div>
