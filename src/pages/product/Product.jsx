463706864fc9ebec79eaf48f8eb7e112d966a298
phantware <phantmoney2011@gmail.com>
2023-05-07 14:08:54 +0100

    Product.js file modified

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -58,7 +58,7 @@ export default function Product() {
     <div className='product'>
       <div className='productTitleContainer'>
         <h1 className='productTitle'>Product hello</h1>
-        <Link to='/newproduct'>
+        <Link to='/products/new'>
           <button className='productAddButton'>Create</button>
         </Link>
       </div>
