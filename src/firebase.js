7bd02efb60c532a1d16ac8b48f883b2a1726b80b
phantware <phantmoney2011@gmail.com>
2023-05-07 13:54:48 +0100

    firebase.js file added

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBgwKNAp4kJweE_R3T5fQoMwJLBQJwHwZg',
+  authDomain: 'phantwear-3a0d2.firebaseapp.com',
+  projectId: 'phantwear-3a0d2',
+  storageBucket: 'phantwear-3a0d2.appspot.com',
+  messagingSenderId: '171341057097',
+  appId: '1:171341057097:web:efc44310c074935fe74398',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
